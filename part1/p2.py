478379201c27ea21837c49273850991ed95ade43
nairx <nairxprv@gmail.com>
2026-01-19 10:48:10 +0530

    updated

---
+++ part1/p2.py
@@ -0,0 +1,12 @@
+name = "John"
+print(name)
+print(type(name))
+age = 21
+print(age)
+print(type(age))
+promoted = True
+print(promoted)
+print(type(promoted))
+score=4.5
+print(score)
+print(type(score))
---
+++ part1/p3.py
@@ -0,0 +1,33 @@
+# age = "10"
+# print(type(age))
+
+# age1 = int(age)
+# print(type(age1))
+
+# age = 10
+# print(type(age))
+
+# age1 = str(age)
+# print(type(age1))
+
+# promoted = "True"
+# print(type(promoted))
+
+# promoted1 = bool(promoted)
+# print(type(promoted1))
+
+# score = "4.5"
+# print(type(score))
+
+# score1 = float(score)
+# print(type(score1))
+
+
+score = "4.5"
+print(type(score))
+
+score1 = eval(score)
+print(type(score1))
+
+
+
