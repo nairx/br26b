654f184b09795af426a045268693b0380ce9b5c3
nairx <nairxprv@gmail.com>
2026-01-28 11:51:54 +0530

    updated

---
+++ part7/p48.py
@@ -0,0 +1,11 @@
+
+from collections import Counter
+wickets = ["John","Amy","Cathy","John","John","Amy"]
+c = Counter(wickets)
+# print(c)
+# print(c.most_common(1))
+print(c.most_common(2))
+
+
+
+
