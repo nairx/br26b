98d3875e003336db1bb84fec5ce7877d03496ad4
nairx <nairxprv@gmail.com>
2026-01-29 14:00:32 +0530

    updated

---
+++ part10/p66.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+score = [45,60,75,90]
+
+# series = pd.Series(score)
+# print(series[0])
+
+series = pd.Series(score,index=["Maths","Sciene","English","Social"])
+print(series)
+print(series["Maths"])
