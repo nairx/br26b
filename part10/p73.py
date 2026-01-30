b3a787e84b12c792f340e64e3a8c845c51352939
nairx <nairxprv@gmail.com>
2026-01-30 12:55:47 +0530

    updated

--- part10/p73.py
+++ part10/p73.py
@@ -10,3 +10,27 @@ data = {
 
 df = pd.DataFrame(data)
 print(df)
+
+mean_sales = np.mean(df["Sales"])
+highest_sales = np.max(df["Sales"])
+lowest_sales = np.min(df["Sales"])
+std_sales = np.std(df["Sales"])
+
+print(f"Average Sales: {mean_sales}")
+print(f"Highest Sales: {highest_sales}")
+print(f"Lowest Sales: {lowest_sales}")
+print(f"Standard Deviation: {std_sales}")
+
+#Identify Best and Worst Month
+
+best_month = df.loc[df["Sales"].idxmax(),"Month"]
+print(best_month)
+best_month = df.loc[df["Sales"].idxmin(),"Month"]
+print(best_month)
+
+plt.plot(df["Month"],df["Sales"])
+plt.xlabel("Year")
+plt.ylabel("Sales")
+plt.title("Sales Trend")
+plt.savefig("salesreport.png")
+plt.show()
